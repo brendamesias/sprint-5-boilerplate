39211ebecbc4837e5de02ad836a9583caad954f1
brendamesias <brendamesiasvillar@gmail.com>
2018-02-21 00:41:34 -0500

    subiendo cambios

--- js/functions.js
+++ js/functions.js
@@ -9,9 +9,7 @@
 //   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 // }
 $(document).ready(function() {
-  // definiendo variables
-  var topicContent = $('.topic-content').val;
-  console.log(topicContent);
+
 
   $.ajax({
     url: 'https://examen-laboratoria-sprint-5.herokuapp.com/topics',
@@ -27,7 +25,44 @@ $(document).ready(function() {
   })
     .done(function(data) {
       for (var i = 0; i < 100; i++) {
-        console.log(data[i]);;
+        var creatingItem = `
+          <div id='item' class="row valign-wrapper topic">
+            <div class="col s4 offset-s2 center-align container-topics">
+                <span class="topic-content">${data[i].content}</span>
+                <br />
+                <span>escrito por: </span>
+                <span class="name">${data[i].author_name}</span>
+            </div>
+            <div class="col s5 left-align container-num-answers">
+              <span class="num-answers">${data[i].responses_count} respuestas</span>
+            </div>
+          </div>
+        `;
+
+        $('#items').append(creatingItem);
       }
     });
+
+
+  // ADD
+  $('#enviarInfo').submit(function(e) {
+    e.preventDefault();
+    console.log($('#icon_prefix').val())
+    console.log($('#icon_prefix2').val())
+
+    $.ajax({
+      url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics',
+      type: 'POST',
+      ContentType: 'application/json',
+      data: {
+        author_name: $('#icon_prefix').val(),
+        content: $('#icon_prefix2').val(),
+      }
+    })
+      .done(function(data) {
+        console.log(data);
+      })
+  })
+
+
 });
